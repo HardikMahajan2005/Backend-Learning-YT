69d2abf1524ef8f51c013dcdfcf9afdbd7374d76
hardik11june2005 <hardikmahajam@gmail.com>
2025-08-01 14:08:32 +0530

    .\11_NPM\

---
+++ 11_NPM/index.js
@@ -0,0 +1,9 @@
+// console.log("jashqsksnkkjebfrgbvbwekjdgbhktbgbfr")
+import http from 'http';
+
+const server=http.createServer((req,res)=>{
+    res.writeHead(202,"Good",{"Content-Type": "text/html"});
+    res.write("<h1>Hello From Node.js Server</h1>");
+});
+
+server.listen(8000,()=>console.log("server Up!"));
